b3fd1dbf20645cfc821d6e5a3f72f1f24e3116c7
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-08-25 15:22:32 +0100

    Clarified the formatting of the tests to follow Arrange-Act-Assert.

--- Ruby/spec/shopping_cart_spec.rb
+++ Ruby/spec/shopping_cart_spec.rb
@@ -3,13 +3,17 @@ require_relative "../lib/shopping_cart"
 describe "ShoppingCart" do
   it "may just have a single item" do
     shopping_cart = ShoppingCart.new
+
     shopping_cart.add(10)
+
     expect(shopping_cart.number_of_products).to eq(1)
   end
 
   it "has the total price equal to total price of its contents" do
     shopping_cart = ShoppingCart.new
+
     shopping_cart.add(10)
+
     expect(shopping_cart.calculate_total_price).to eq(10)
   end
 
@@ -26,7 +30,9 @@ describe "ShoppingCart" do
 
   it "has discount when the shopping card contains exactly one premium item over 100 currency units" do
     shopping_cart = ShoppingCart.new
+
     shopping_cart.add(100)
+
     expect(shopping_cart.has_discount?).to be true
   end
 
@@ -41,14 +47,18 @@ describe "ShoppingCart" do
 
   it "does not have discount when the cart contains exactly one cheap (< 100) item of shopping" do
     shopping_cart = ShoppingCart.new
+
     shopping_cart.add(10)
+
     expect(shopping_cart.has_discount?).to be false
   end
 
   it "does not have discount when the cart contains multiple cheap (< 100) items of shopping" do
     shopping_cart = ShoppingCart.new
+
     shopping_cart.add(10)
     shopping_cart.add(99)
+
     expect(shopping_cart.has_discount?).to be false
   end
 end
